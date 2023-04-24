2a5a9f211ec0b0594b003d8b7ea70ea7f63047bb
k3nny <k3nny.wx@mailfence.com>
2023-04-24 12:21:37 +0800

    code cleanup

--- lib.h
+++ lib.h
@@ -68,39 +68,3 @@ arg_val (int hist_all, char *logdir, char *pkg_name, int logvar)
 
   return 0;
 }
-
-
-/*
-void
-changemode (int dir)
-{
-  static struct termios oldt, newt;
-
-  if (dir == 1)
-    {
-      tcgetattr (STDIN_FILENO, &oldt);
-      newt = oldt;
-      newt.c_lflag &= ~(ICANON | ECHO);
-      tcsetattr (STDIN_FILENO, TCSANOW, &newt);
-    }
-  else
-    tcsetattr (STDIN_FILENO, TCSANOW, &oldt);
-}
-
-int
-kbhit (void)
-{
-  struct timeval tv;
-  fd_set rdfs;
-
-  tv.tv_sec = 0;
-  tv.tv_usec = 0;
-
-  FD_ZERO (&rdfs);
-  FD_SET (STDIN_FILENO, &rdfs);
-
-  select (STDIN_FILENO + 1, &rdfs, NULL, NULL, &tv);
-  return FD_ISSET (STDIN_FILENO, &rdfs);
-
-}
-*/
